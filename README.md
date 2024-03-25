1e346be77c31f6f8c644c80d15ce5a4e697131cf
Killar.exe <killar.exe@gmail.com>
2024-03-25 20:34:18 +0100

    Add setup.sh script

--- README.md
+++ README.md
@@ -12,32 +12,10 @@ git clone --recurse-submodules https://github.com/Killarexe/.dotfiles.git
 cd .dotfiles
 ```
 
-2. Install the requirements/dependencies
-
-Install Chaotic-AUR with those commands:
+2. Execute setup.sh 
 
 ```
-pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key 3056513887B78AEB
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+sh setup.sh
 ```
 
-And add thoses lines at the end of the file `/etc/pacman.conf`:
-
-```
-[chaotic-aur]
-Include = /etc/pacman.d/chaotic-mirrorlist
-```
-
-To finish. Execute this command to install all requirements:
-```
-pacman -Syu - < pkglist.txt
-```
-
-3. Once cloned use `stow` to link the .dotfiles...
-
-```
-cd .dotfiles
-stow .
-```
+Done!
---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+echo "Adding chaotic-aur..."
+
+echo "\tAdding server keys..."
+{
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+} &> /dev/null
+
+echo "\tDownloading pkg list..."
+{
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+} &> /dev/null
+
+echo "\tEditing /etc/pacman.conf..."
+STR='[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist'
+
+if [[ -f /etc/pacman.conf ]]; then
+  echo $STR >> /etc/pacman.conf
+else
+  echo "\t\tError: /etc/pacman.conf does not exist! Check if pacman is downloaded..."
+fi
+
+echo "Downloading pakages..."
+sudo pacman -Syu - < pkglist.txt &> /dev/null
+
+echo "Stow dotfiles..."
+stow .
+
+echo "Done!"
